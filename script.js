cd7dda4a40451a468e7d099d4d0fb0da9c2d8543
Mansé <sd.arthur@gmail.com>
2025-12-20 18:02:17 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,90 @@
+async function loadWeather() {
+    try {
+  
+// DIA DA SEMANA
+
+  const dias = [
+    "domingo",
+    "segunda-feira",
+    "terça-feira",
+    "quarta-feira",
+    "quinta-feira",
+    "sexta-feira",
+    "sábado"
+  ];
+
+  const hoje = new Date();
+
+  document.getElementById("diaSemana").textContent = dias[hoje.getDay()];
+
+// TEMPERATURA
+
+  const API_KEY = "ff54cdc13d1bf57ebe1d84b71cb8b896";
+  const CITY = "João Pessoa,BR";
+
+  const url = 
+        `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(CITY)}&appid=${API_KEY}&units=metric&lang=pt_br`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const temp = Math.round(data.main.temp);
+  document.getElementById("tempClima").textContent = `${temp} °C`;
+
+  // ICONES
+
+  const iconClima = {
+    Clear: {
+      day: "./img/clima/sun.png",
+      night: "./img/clima/moon.png"
+    },
+    Clouds: {
+      day: "./img/clima/dia-nublado.png",
+      night: "./img/clima/noite-nublado.png"
+    },
+    Rain: {
+      day: "./img/clima/chuva.png",
+      night: "./img/clima/chuva.png"
+    },
+    Storm: {
+      day: "./img/clima/storm.png",
+      night: "./img/clima/storm.png"
+    }
+  };
+
+  const agora = data.dt;
+  const nascerSol = data.sys.sunrise;
+  const porSol = data.sys.sunset;
+
+  const isDay = agora >= nascerSol && agora < porSol;
+  const condicao = data.weather[0].main;
+
+  if (!iconClima[condicao]) {
+  condicao = "Clouds"; 
+  }
+
+  const icon = isDay ? iconClima[condicao].day : iconClima[condicao].night;
+
+  document.getElementById("imagemClima").src = icon;
+
+  } catch (error) {
+      console.error("Erro ao carregar dados do clima:", error);
+  }
+};
+
+
+loadWeather();
+setInterval(loadWeather, 300000);
+
+// CONTEÚDO:
+
+const headers = document.querySelectorAll('.expande');
+const fundo = document.querySelector('.fundo');
+const setas = document.querySelectorAll('.icone_seta');
+
+setas.forEach(seta => {
+  seta.addEventListener('click', e => {
+    e.stopPropagation();
+    fundo.focus();
+  });
+});
