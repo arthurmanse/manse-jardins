88403d20a44429c7d3be4d0e3fc0dca35cf64569
Mansé <sd.arthur@gmail.com>
2026-02-03 20:25:05 -0300

    teste erro 500

--- api/weather.js
+++ api/weather.js
@@ -1,15 +1,6 @@
-export default async function handler(req, res) {
-  const city = "João Pessoa,BR";
-  const apiKey = process.env.OPENWEATHER_KEY;
-
-  const url = 
-        `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(CITY)}&appid=${API_KEY}&units=metric&lang=pt_br`;`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar clima" });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    keyExists: !!process.env.OPENWEATHER_KEY
+  });
 }
